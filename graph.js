958edfd897a0d02157a5f07d4cf9c66189bf26ab
Luis Irizarry <lirizarry6178@gmail.com>
2025-03-13 17:17:31 -0400

    finished exercise

--- graph.js
+++ graph.js
@@ -11,25 +11,81 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    if (this.nodes.has(v1) && this.nodes.has(v2)) {
+      v1.adjacent.add(v2);
+      v2.adjacent.add(v1);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    if (this.nodes.has(v1) && this.nodes.has(v2)) {
+      v1.adjacent.delete(v2);
+      v2.adjacent.delete(v1);
+    }
+  }
 
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  // this function accepts a vertex and removes it from the nodes property,
+  // it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    if (this.nodes.has(vertex)) {
+      for (let neighbor of vertex.adjacent) {
+        neighbor.adjacent.delete(vertex);
+      }
+      this.nodes.delete(vertex);
+    }
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    function dfs(node) {
+      if (!node || visited.has(node)) return;
+      visited.add(node);
+      result.push(node.value);
+      for (let neighbor of node.adjacent) {
+        dfs(neighbor);
+      }
+    }
+
+    dfs(start);
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+    const queue = [start];
+
+    while (queue.length) {
+      const node = queue.shift();
+      if (!visited.has(node)) {
+        visited.add(node);
+        result.push(node.value);
+        for (let neighbor of node.adjacent) {
+          if (!visited.has(neighbor)) queue.push(neighbor);
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
-module.exports = {Graph, Node}
+module.exports = { Graph, Node };
